8bb1b96b9b6df922afdd5a0c66284766a2fcfd5f
albertpiacka <albertpiacka@gmail.com>
2020-09-22 19:56:21 +0200

    _add-post.scss mierne upravy dizajnu

--- add-post.php
+++ add-post.php
@@ -1,23 +1,25 @@
 <?php include_once('_partials/header.php') ?>
 
-                <form id="myForm" action="_inc/new-post.php" method="post">
-                    <textarea name="message" id="message" cols="30" rows="10" placeholder="type"></textarea>
-                    <input type="submit" placeholder="submit">
-                </form>
+                <div class="form-container">
+                    <form id="myForm" action="_inc/new-post.php" method="post">
+                        <textarea name="message" id="message" cols="30" rows="10" placeholder="Type something"></textarea>
+                        <input type="submit" placeholder="Send">
+                    </form>
 
-                <div>
-                    <ul id="posts">
-                        <?php
-                            $posts = $DB->query("SELECT * FROM posts");
+                    <div>
+                        <ul id="posts">
+                            <?php
+                                $posts = $DB->query("SELECT * FROM posts");
 
-                            foreach($posts as $post){
-                                echo '<li>';
-                                echo $post['text'];
-                                echo '<li>';
-                            }
+                                foreach($posts as $post){
+                                    echo '<li>';
+                                    echo $post['text'];
+                                    echo '<li>';
+                                }
 
-                        ?>
-                    </ul>
+                            ?>
+                        </ul>
+                    </div>
                 </div>
 			</div>
 
