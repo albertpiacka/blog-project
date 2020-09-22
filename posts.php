5f3ff8ffbd3cc5d006fb907ae5225d57059939e8
albertpiacka <albertpiacka@gmail.com>
2020-09-22 18:02:25 +0200

    posts.php uz nema formular

--- posts.php
+++ posts.php
@@ -39,22 +39,6 @@
 									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 								</p>
 							</div>
-
-							<form action="new-post.php" method="post">
-								<textarea name="message" id="message" cols="30" rows="10" placeholder="type"></textarea>
-								<input type="submit" placeholder="submit">
-							</form>
-
-							<div class="fetch-data">
-								<ul>
-									<?php
-										$posts = $DB->query("SELECT * FROM posts");
-
-										var_dump($posts->fetchAll());
-
-									?>
-								</ul>
-							</div>
 						</div>
 
 						<div class="post post-2">
