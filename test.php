ceb4972f5b5cb419cf495d9502b027d412a57270
albertpiacka <albertpiacka@gmail.com>
2020-09-30 15:04:30 +0200

    vytvoril som strukturu prototypu postu

--- test.php
+++ test.php
@@ -3,15 +3,38 @@
 				<section id="post-wrap">
                     <header>
                         <h1>How to cook bull balls</h1>
-                        <div class="date">12 July 2019</div>
+                        <div class="post-date">12 July 2019</div>
                         <div class="author">
                             <span>Author</span>
                             <span class="author-name">Palo</span>
                         </div>
                     </header>
 
-                    <main></main>
-                    <footer></footer>
+                    <main>
+                        <div class="img-container"></div>
+                        <div class="text-wrapper">
+                            <p></p>
+                        </div>
+
+                    </main>
+
+                    <footer>
+                        <div class="comments">
+                            <div class="comment">
+                                <div class="comment-header">
+                                    <div class="author">
+                                        <i class="fas fa-user-alt"></i>
+                                        <a href="#">Filip</a>
+                                    </div>
+                                    <div class="comment-date">24 July 2019</div>
+                                </div>
+
+                                <div class="comment-body">
+                                    Bla bla bla nejake picoviny.. 
+                                </div>
+                            </div>
+                        </div>
+                    </footer>
                 </section>
 			</div>
 
@@ -19,4 +42,5 @@
 
 
 <?php include_once('_partials/footer.php') ?>
+
 
