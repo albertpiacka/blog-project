85b860c1f542cec88e7b55296d293c4bc5eb6800
albertpiacka <albertpiacka@gmail.com>
2020-09-30 14:34:37 +0200

    zmena pre velkost  h1

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php include_once('_partials/header.php') ?>
+
+				<section id="post-wrap">
+                    <header>
+                        <h1>How to cook bull balls</h1>
+                        <div class="date">12 July 2019</div>
+                        <div class="author">
+                            <span>Author</span>
+                            <span class="author-name">Palo</span>
+                        </div>
+                    </header>
+
+                    <main></main>
+                    <footer></footer>
+                </section>
+			</div>
+
+		</div>
+
+
+<?php include_once('_partials/footer.php') ?>
+
