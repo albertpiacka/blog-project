ecf01bdabf0d2915e2b8e6ce2a4bad26928e5bd6
albertpiacka <albertpiacka@gmail.com>
2020-09-30 17:09:26 +0200

    vytvoril som strukturu pre comment

--- posts.php
+++ posts.php
@@ -2,7 +2,7 @@
 
 				<section class="post-section">
 					<div class="post-wrapper">
-						
+
 						<div class="post post-1">
 							<div class="img-container">
 								<div style="background-image: url('assets/img/img-1.jpg')" class="img"></div>
@@ -13,9 +13,9 @@
 									<input type="text" placeholder="Search">
 								</div>
 
-								<div class="post-thumbnail">Article</div>
-								<div class="post-thumbnail">Article</div>
-								<div class="post-thumbnail">Article</div>
+								<div class="post-thumbnail">#Poached madness</div>
+								<div class="post-thumbnail">How to ferment anything</div>
+								<div class="post-thumbnail">Don't eat beavers</div>
 							</div>
 
 							<div class="header-container">
@@ -26,7 +26,7 @@
 
 								<div class="post-author">
 									<span>Author</span>
-									<span class="author-name">John</span>
+									<span class="author-name">Jane</span>
 								</div>
 							</div>
 
@@ -57,14 +57,14 @@
 									<input type="text" placeholder="Search">
 								</div>
 
-								<div class="post-thumbnail">Article</div>
-								<div class="post-thumbnail">Article</div>
-								<div class="post-thumbnail">Article</div>
+								<div class="post-thumbnail">#Poached madness</div>
+								<div class="post-thumbnail">How to ferment anything</div>
+								<div class="post-thumbnail">Don't eat beavers</div>
 							</div>
 
 							<div class="header-container">
 								<div class="title">
-									<h1>#Poached madness</h1>
+									<h1>How to ferment anything</h1>
 									<div class="post-date">12 July 2019</div>
 								</div>
 
@@ -88,6 +88,22 @@
 									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 								</p>
 							</div>
+
+							<div class="comments">
+								<div class="comment-header">
+									<div class="comment-author">
+										<i class="fa fa-user"></i>
+										<a href="#">Name</a>
+									</div>
+									<div class="comment-date">
+										<span>12 July 2019</span>
+									</div>
+								</div>	
+
+								<div class="comment-body">
+									<p>Nejaké sračky, akože komenty..</p>
+								</div>
+							</div>
 						</div>
 					</div>
 
@@ -96,9 +112,9 @@
 							<input type="text" placeholder="Search">
 						</div>
 
-						<div class="post-thumbnail">Article</div>
-						<div class="post-thumbnail">Article</div>
-						<div class="post-thumbnail">Article</div>
+						<div class="post-thumbnail">#Poached madness</div>
+						<div class="post-thumbnail">How to ferment anything</div>
+						<div class="post-thumbnail">Don't eat beavers</div>
 					</div>
 				</section>
 			</div>
