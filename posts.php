9cb1928d574e5744969b15d102c0a995d5189000
albertpiacka <albertpiacka@gmail.com>
2020-09-30 15:23:43 +0200

    vymazal som test.php a test.scss, upravil som post v posts.php

--- posts.php
+++ posts.php
@@ -2,10 +2,12 @@
 
 				<section class="post-section">
 					<div class="post-wrapper">
+						
 						<div class="post post-1">
 							<div class="img-container">
 								<div style="background-image: url('assets/img/img-1.jpg')" class="img"></div>
 							</div>
+
 							<div class="post-menu secondary">
 								<div class="search-box">
 									<input type="text" placeholder="Search">
@@ -15,16 +17,19 @@
 								<div class="post-thumbnail">Article</div>
 								<div class="post-thumbnail">Article</div>
 							</div>
+
 							<div class="header-container">
-								<h3>#Poached madness</h2>
-								<div class="author">
-									<i class="far fa-user"></i>
-									<div class="author-name">
-										<span>Author</span>
-										<span>John</span>
-									</div>
+								<div class="title">
+									<h1>#Poached madness</h1>
+									<div class="post-date">12 July 2019</div>
+								</div>
+
+								<div class="post-author">
+									<span>Author</span>
+									<span class="author-name">John</span>
 								</div>
 							</div>
+
 							<div class="paragraphs">
 								<p>
 									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -42,9 +47,11 @@
 						</div>
 
 						<div class="post post-2">
+
 							<div class="img-container">
 								<div style="background-image: url('assets/img/img-2.jpg')" class="img"></div>
 							</div>
+
 							<div class="post-menu secondary">
 								<div class="search-box">
 									<input type="text" placeholder="Search">
@@ -54,16 +61,19 @@
 								<div class="post-thumbnail">Article</div>
 								<div class="post-thumbnail">Article</div>
 							</div>
+
 							<div class="header-container">
-								<h3>#Poached madness</h2>
-								<div class="author">
-									<i class="far fa-user"></i>
-									<div class="author-name">
-										<span>Author</span>
-										<span>John</span>
-									</div>
+								<div class="title">
+									<h1>#Poached madness</h1>
+									<div class="post-date">12 July 2019</div>
+								</div>
+
+								<div class="post-author">
+									<span>Author</span>
+									<span class="author-name">John</span>
 								</div>
 							</div>
+
 							<div class="paragraphs">
 								<p>
 									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
