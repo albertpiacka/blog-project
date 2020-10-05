f65a10c3be1fb7c1378f3cc9c6b4e48ecbaae93f
albertpiacka <albertpiacka@gmail.com>
2020-10-05 13:31:33 +0200

    Ak neexistuje ziadny post, zobrazi sa message flash message

--- posts.php
+++ posts.php
@@ -9,33 +9,37 @@
 
 								$posts = $query->fetchAll();
 
-								foreach($posts as $post){
-									echo '<div class="post post-'.$post['id'].'">';
-									echo 	'<div class="img-container">';
-									echo 		'<div class="img"></div>';
-									echo	'</div>';
-
-									echo 	'<div class="post-container">';
-                                    echo 		'<div class="post-header">';
-                                    echo     		'<div class="post-title">';
-                                    echo        		'<h1>'.$post['title'].'</h1>';
-                                    echo 				'<span>'.$post['created_at'].'</span>';
-                                    echo     		'</div>';
-                                    echo 			'<div class="post-author">';
-                                    echo				'<h2>Author</h2>';
-                                    echo			'</div>';
-                                    echo		'</div>';
-
-                                    echo		'<div class="post-body">';
-                                    echo     		'<div class="post-text">';
-                                                		$str = $post['text'];
-                                                		$oldArr = explode(' ', $str);
-                                                		split_array($oldArr);
-                                    echo     		'</div>';
-									echo 		'</div>';
-									echo 	'</div>';
-                                    echo '</div>';
-                                }
+								if(!$posts){
+									echo "<p>I'm so empty :-(</p>";
+								} else {
+									foreach($posts as $post){
+										echo '<div class="post post-'.$post['id'].'">';
+										echo 	'<div class="img-container">';
+										echo 		'<div class="img"></div>';
+										echo	'</div>';
+	
+										echo 	'<div class="post-container">';
+										echo 		'<div class="post-header">';
+										echo     		'<div class="post-title">';
+										echo        		'<h1>'.$post['title'].'</h1>';
+										echo 				'<span>'.$post['created_at'].'</span>';
+										echo     		'</div>';
+										echo 			'<div class="post-author">';
+										echo				'<h2>Author</h2>';
+										echo			'</div>';
+										echo		'</div>';
+	
+										echo		'<div class="post-body">';
+										echo     		'<div class="post-text">';
+															$str = $post['text'];
+															$oldArr = explode(' ', $str);
+															split_array($oldArr);
+										echo     		'</div>';
+										echo 		'</div>';
+										echo 	'</div>';
+										echo '</div>';
+									}
+								}
 							?>
 						</div>
 						
