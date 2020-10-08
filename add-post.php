4ebd460c215848ded24b8d5c561b6834b5f4d19c
albertpiacka <albertpiacka@gmail.com>
2020-10-08 15:06:50 +0200

    zvacsil som textarea rows na 20

--- add-post.php
+++ add-post.php
@@ -5,7 +5,7 @@
                     <!-- formular na spracovanie udajov -->
                     <form id="add-form" action="_inc/new-post.php" method="post">
                         <input name="title" type="text" placeholder="Title" id="title">
-                        <textarea name="message" id="message" cols="30" rows="10" placeholder="Type something"></textarea>
+                        <textarea name="message" id="message" cols="30" rows="20" placeholder="Type something"></textarea>
                         <input type="submit">
                     </form>
 
