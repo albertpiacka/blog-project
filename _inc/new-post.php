b05dd37bf282a8d2d694779e1d82b75cdd86ca46
albertpiacka <albertpiacka@gmail.com>
2020-10-11 15:40:50 +0200

    success flash message a odteraz aj vkladanie obrazkov

--- _inc/new-post.php
+++ _inc/new-post.php
@@ -4,11 +4,13 @@
 
     $message = $_POST['message'];
     $title = $_POST['title'];
+    $img = $_POST['img_url'];
 
-    $id = $DB->query("INSERT INTO posts (text, title) VALUES ('$message', '$title')");
+    $id = $DB->query("INSERT INTO posts (text, title, img_url) VALUES ('$message', '$title', '$img')");
 
     if($id){
         // $json = json_encode([$message, $title]);
+        $msg->success('The post was succesfully added!');
         header('Location: '.BASE_URL.'posts.php');
         die(); //tento json obsahuje vsetko co sa odosiela , pozri vyssie.. v javascripte k hodnotam pristupujeme pomocou indexu
     }
