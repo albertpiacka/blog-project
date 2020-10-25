7163f435decdc0809048cb80e05c7931f7a1e764
albertpiacka <albertpiacka@gmail.com>
2020-10-25 13:41:56 +0100

    v clankoch sa uz zobrazuju mena podla autorova dynamicky pridavane obrazky

--- index.php
+++ index.php
@@ -18,16 +18,22 @@
 								echo '<article class="article article-'.$post['id'].'">';
 								echo 	'<div class="header-container">';
 								echo		'<div class="title">';
-								echo			'<h1>'.$post['title'].'</h1>';
+								echo			'<a href="'.BASE_URL.'posts.php#post-'.$post['id'].'"><h1>'.$post['title'].'</h1></a>';
 								echo		'</div>';
 								echo		'<div class="post-author">';
 								echo			'<span>Author</span>';
-								echo			'<span class="author-name">Name</span>';
+								$id = $post['user_id'];
+								$queryUsers = $dbh->query("SELECT * FROM phpauth_users WHERE id = $id");
+
+								$users = $queryUsers->fetchAll();
+								if($post['user_name'] == 'empty'){
+									echo			'<span class="author-name">'.$users[0]['email'].'</span>';
+								} else echo 		'<span class="author-name">'.$post['user_name'].'</span>';
 								echo		'</div>';
 								echo	'</div>';
 
 								echo 	'<div class="img-container">';
-								echo		'<img src="'.$post['img_url'].'" alt="">';
+								echo		'<a href="'.BASE_URL.'posts.php#post-'.$post['id'].'"><img src="files/'.$post['img_dir'].'" alt=""></a>';
 								echo	'</div>';
 								echo	'<p>';
 								echo		$post['text'];
